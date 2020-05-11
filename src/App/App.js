0f762490e5de0e57896e96ae6e11f9ba5465ab8c
Liza Star <lizadstar18@gmail.com>
2020-05-11 18:56:29 -0500

    goat data loads

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import './App.scss';
+import goatData from '../helpers/data/goatData';
 
-function App() {
-  return (
-    <div className="App">
-      <h1> I am a H1 </h1>
-   <button className="btn btn-danger"> I am a button</button>
-    </div>
-  );
+class App extends React.Component {
+  // outside render - anything that modifies state
+
+  componentDidMount() {
+    const goats = goatData.getGoats();
+    console.error('goats', goats);
+  }
+
+  render() {
+    // inside the render - anything we need to do to modify the UI
+
+    return (
+      <div className="App">
+        <h2 className='goat-title'>Goats</h2>
+        <button className="btn btn-success"><i className="fas fa-tree fa-3x"></i></button>
+      </div>
+    );
+  }
 }
 
 export default App;
